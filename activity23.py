5fb202ed9746f9f0cc661f9c6198b37c4cb1a863
Ken Gimelson B. Solo <93087037+Unkn0wnM3me@users.noreply.github.com>
2025-11-30 06:35:10 +0800

    Create activity23.py

---
+++ activity23.py
@@ -0,0 +1,12 @@
+from activity23_1 import GreetWithName, GreetPerson, FunctionWithReturn, Factorial
+#Defining a function
+def MyFirstFunction():
+    print("Hello Guys")
+    print("Hi")
+
+MyFirstFunction()   
+GreetWithName('Ken')
+GreetWithName('Solo')
+GreetPerson('Dash', 'Sariaya', '18')
+print(f"I want to get the summation of {FunctionWithReturn(10)}")
+print(f"The factorial of {Factorial(6)}")
