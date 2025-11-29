0cd7fa819fff83b5eb9a3adbe81c023ba17da4ab
Ken Gimelson B. Solo <93087037+Unkn0wnM3me@users.noreply.github.com>
2025-11-30 06:40:23 +0800

    Create activity25.py

---
+++ activity25.py
@@ -0,0 +1,16 @@
+#LIST
+
+programming_language = ['java', 'C#', 'C++', 'C', 'Perl', 'Python', 'JavaScript', 'Rust', 'GoLang']
+
+print(programming_language)
+
+#DICTIONARY
+
+emtpy_dictionary = {} #empty dictionary
+# { KEY  VALUE } : dictionary has no index, it uses 'KEYS'
+
+pl_dictionary = {'ken' : 'java', 'clark' : 'C#', 'soli' : 'C++',\
+                  'panget' : 'C', 'pogi' : 'Perl', 'lim' : 'Python',\
+                      'minecraft' : 'JavaScript', 'lol' : 'Rust', 'estilo' : 'Getgood'}
+
+print(pl_dictionary['ken'])           
